c94b139968da249b3d141fe505af5a9b32b76388
amzamzamzamz <43040644+amzamzamzamz@users.noreply.github.com>
2018-09-12 00:49:41 +0500

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -21,6 +21,17 @@ service avreg stop
 service apache2 stop
 service mysql stop
 
+# create MEDIADIR and mapping
+MEDIADIR=/avreg_media
+if [ ! -f $MEDIADIR/initialized ]; then
+	mv /var/spool/avreg* $MEDIADIR
+	touch $MEDIADIR/initialized	
+fi
+rm -rf /var/spool/avreg
+ln -s $MEDIADIR /var/spool/avreg
+chown root:avreg /var/spool/avreg
+
+# create DBDIR and mapping
 DBDIR=/avreg_db
 if [ ! -f $DBDIR/initialized ]; then
 	mv /var/lib/mysql/avreg6_db/* $DBDIR
